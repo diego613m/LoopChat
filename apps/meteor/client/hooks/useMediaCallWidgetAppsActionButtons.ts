b4e59885387023ae2b0e8bc37d0472589c3f90f7
Douglas Gubert <douglas.gubert@gmail.com>
2026-05-28 14:30:58 -0300

    feat: adapt meteor media call to get the action buttons
    fix handle interaction

--- apps/meteor/client/hooks/useMediaCallWidgetAppsActionButtons.ts
+++ apps/meteor/client/hooks/useMediaCallWidgetAppsActionButtons.ts
@@ -1,52 +1,41 @@
-import type { IUIActionButton } from '@rocket.chat/apps-engine/definition/ui';
 import '@rocket.chat/apps-engine/experimental/MediaCallActionButtons';
-import type { IRoom } from '@rocket.chat/core-typings';
 import { useToastMessageDispatch } from '@rocket.chat/ui-contexts';
 import type { ActionButtonUpdatePayload } from '@rocket.chat/ui-contexts/dist/ActionManagerContext';
 import type {
 	MediaCallAppActionDescriptor,
 	MediaCallAppActionsContextValue,
 } from '@rocket.chat/ui-voip/dist/experimental/AppActionButtons';
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useAppActionButtons } from './useAppActionButtons';
-import { applyRoomFilter, useApplyButtonAuthFilter } from './useApplyButtonFilters';
+import { useApplyButtonAuthFilter } from './useApplyButtonFilters';
 import { UiKitTriggerTimeoutError } from '../../app/ui-message/client/UiKitTriggerTimeoutError';
 import { Utilities } from '../../ee/lib/misc/Utilities';
 import { useUiKitActionManager } from '../uikit/hooks/useUiKitActionManager';
 
-type Options = {
-	room?: IRoom;
-};
-
-const useApplyMediaCallWidgetButtonFilters = (room?: IRoom): ((button: IUIActionButton) => boolean) => {
-	const applyAuthFilter = useApplyButtonAuthFilter();
-	return useCallback(
-		(button: IUIActionButton) => applyAuthFilter(button, room) && (!room || applyRoomFilter(button, room)),
-		[applyAuthFilter, room],
-	);
-};
-
-export const useMediaCallWidgetAppsActionButtons = ({ room }: Options) => {
-	const { data } = useAppActionButtons('mediaCallWidgetAction');
+export const useMediaCallWidgetAppsActionButtons = () => {
 	const actionManager = useUiKitActionManager();
-	const applyButtonFilters = useApplyMediaCallWidgetButtonFilters(room);
 	const { t } = useTranslation();
 	const dispatchToastMessage = useToastMessageDispatch();
+	const applyAuthFilter = useApplyButtonAuthFilter();
+	const { data } = useAppActionButtons('mediaCallWidgetAction');
 
 	return useMemo<MediaCallAppActionsContextValue>(
 		() => ({
 			actions:
-				data?.filter(applyButtonFilters).map(
-					(action): MediaCallAppActionDescriptor => ({
-						label: t(Utilities.getI18nKeyForApp(action.labelI18n, action.appId)),
-						variant: action.variant,
-						appId: action.appId,
-						actionId: action.actionId,
-						...(action.when?.callStates ? { callStates: action.when.callStates } : {}),
-					}),
-				) || [],
+				data
+					?.filter((button) => applyAuthFilter(button))
+					.map(
+						(action): MediaCallAppActionDescriptor => ({
+							label: t(Utilities.getI18nKeyForApp(action.labelI18n, action.appId)),
+							variant: action.variant,
+							appId: action.appId,
+							actionId: action.actionId,
+							...(action.when?.callStates ? { callStates: action.when.callStates } : {}),
+						}),
+					) || [],
+
 			handleInteraction: async ({ button, sessionState }) => {
 				const { promise, resolve } = Promise.withResolvers<Awaited<ReturnType<MediaCallAppActionsContextValue['handleInteraction']>>>();
 				const updateHandler: (data: ActionButtonUpdatePayload) => void = ({ appId, actionId, update }) => {
@@ -54,8 +43,6 @@ export const useMediaCallWidgetAppsActionButtons = ({ room }: Options) => {
 						return;
 					}
 
-					actionManager.off('action_button.update', updateHandler);
-					clearTimeout(timeoutId);
 					resolve({
 						update: {
 							...(update.labelI18n && { label: t(Utilities.getI18nKeyForApp(update.labelI18n, appId)) }),
@@ -67,11 +54,6 @@ export const useMediaCallWidgetAppsActionButtons = ({ room }: Options) => {
 				};
 
 				actionManager.on('action_button.update', updateHandler);
-
-				const timeoutId = setTimeout(() => {
-					actionManager.off('action_button.update', updateHandler);
-					resolve();
-				}, 5000);
 
 				await actionManager
 					.emitInteraction(button.appId, {
@@ -91,11 +73,12 @@ export const useMediaCallWidgetAppsActionButtons = ({ room }: Options) => {
 						}
 
 						dispatchToastMessage({ type: 'error', message: t('An_error_occurred_while_executing_the_action') });
-					});
+					})
+					.finally(() => resolve({ update: { disabled: false } }));
 
 				return promise;
 			},
 		}),
-		[actionManager, applyButtonFilters, data, dispatchToastMessage, t],
+		[actionManager, applyAuthFilter, data, dispatchToastMessage, t],
 	);
 };
