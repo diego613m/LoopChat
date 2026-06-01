16ff90a23a8f38bc946781e5e239471f15e960a2
Douglas Gubert <douglas.gubert@gmail.com>
2026-06-01 17:19:55 -0300

    refactor: adapt mocked providers to new structure

--- packages/ui-voip/src/experimental/AppActionButtons/providers/MockedMediaCallAppActionsProvider.tsx
+++ packages/ui-voip/src/experimental/AppActionButtons/providers/MockedMediaCallAppActionsProvider.tsx
@@ -1,51 +1,49 @@
-import type { MediaCallAppActionsProviderProps } from './MediaCallAppActionsProvider';
-import { MediaCallInstanceContext, useMediaCallInstance } from '../../../context';
+import type { MediaCallInstanceContextValue } from '../../../context/MediaCallInstanceContext';
+import MockedMediaCallProvider, { type MockedMediaCallProviderProps } from '../../../providers/MockedMediaCallProvider';
 
-const MockedMediaCallAppActionsProvider = ({ children, actions, handleInteraction }: Partial<MediaCallAppActionsProviderProps>) => {
-	const instanceContextValue = useMediaCallInstance();
+export type MockedMediaCallAppActionsProviderProps = MockedMediaCallProviderProps & Partial<MediaCallInstanceContextValue['appActions']>;
+type HandleInteractionReturn = Awaited<ReturnType<NonNullable<MediaCallInstanceContextValue['appActions']>['handleInteraction']>>;
 
+const MockedMediaCallAppActionsProvider = ({ children, actions, handleInteraction, ...rest }: MockedMediaCallAppActionsProviderProps) => {
 	return (
-		<MediaCallInstanceContext.Provider
-			value={{
-				...instanceContextValue,
-				appActions: {
-					actions: actions || [
-						{
-							appId: 'app-id',
-							actionId: 'change-label',
-							label: 'Click to change label',
-						},
-						{
-							appId: 'app-id',
-							actionId: 'change-variant',
-							label: 'Click to change label AND variant',
-						},
-					],
-					handleInteraction:
-						handleInteraction ||
-						(async ({ button, sessionState }) => {
-							console.log(`Action clicked in call state`, { button, sessionState });
-							const { promise, resolve } =
-								Promise.withResolvers<Awaited<ReturnType<MediaCallAppActionsProviderProps['handleInteraction']>>>();
-							setTimeout(() => {
-								switch (button.actionId) {
-									case 'change-label':
-										resolve({ update: { label: 'Label changed!' } });
-										break;
-									case 'change-variant':
-										resolve({ update: { label: 'Variant changed!', variant: 'danger' } });
-										break;
-									default:
-										resolve({ update: {} });
-								}
-							}, 500);
-							return promise;
-						}),
-				},
+		<MockedMediaCallProvider
+			{...rest}
+			appActions={{
+				actions: actions || [
+					{
+						appId: 'app-id',
+						actionId: 'change-label',
+						label: 'Click to change label',
+					},
+					{
+						appId: 'app-id',
+						actionId: 'change-variant',
+						label: 'Click to change label AND variant',
+					},
+				],
+				handleInteraction:
+					handleInteraction ||
+					(async ({ button, sessionState }) => {
+						console.log(`Action clicked in call state`, { button, sessionState });
+						const { promise, resolve } = Promise.withResolvers<HandleInteractionReturn>();
+						setTimeout(() => {
+							switch (button.actionId) {
+								case 'change-label':
+									resolve({ update: { label: 'Label changed!' } });
+									break;
+								case 'change-variant':
+									resolve({ update: { label: 'Variant changed!', variant: 'danger' } });
+									break;
+								default:
+									resolve({ update: {} });
+							}
+						}, 500);
+						return promise;
+					}),
 			}}
 		>
 			{children}
-		</MediaCallInstanceContext.Provider>
+		</MockedMediaCallProvider>
 	);
 };
 
